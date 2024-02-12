26c40674596135e2c4bae7934d64f8b644eb3494
Nathan Whitehead <nwhitehe@gmail.com>
2024-02-12 11:53:21 -0700

    Consolidate tests into one file.

--- test/simple.py
+++ test/simple.py
@@ -1,6 +1,4 @@
 import tinysoundfont
-import numpy as np
-import scipy.io.wavfile
 
 def test_help():
     import os
@@ -12,7 +10,9 @@ def test_load():
     assert sf.get_preset_count() == 1
     assert sf.get_preset_name(0) == 'El Cheapo Organ'
 
-def test_chord():
+def test_wav():
+    import numpy as np
+    import scipy.io.wavfile
     sf = tinysoundfont.SoundFont('test/example.sf2')
     sf.reset()
     sf.set_max_voices(8)
@@ -49,9 +49,50 @@ def test_chord():
     # Write to WAV file
     scipy.io.wavfile.write('test.wav', 44100, output)
 
+def test_blocking():
+    import pyaudio
+    import numpy as np
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(4),
+                    channels=2,
+                    rate=44100,
+                    output=True)
+    sf = tinysoundfont.SoundFont('test/example.sf2')
+    sf.reset()
+    sf.set_max_voices(8)
+    sf.set_output(tinysoundfont.OutputMode.StereoInterleaved, 44100, -18.0)
+    sf.set_channel_preset_index(0, 0)
+    buffer = np.zeros((44100, 2), dtype=np.float32)
+
+    # 1 second silence
+    sf.render(buffer)
+    stream.write(buffer.astype(np.float32).tobytes())
+
+    # 1 second chord
+    sf.channel_note_on(0, 48, 1.0)
+    sf.channel_note_on(0, 52, 1.0)
+    sf.channel_note_on(0, 55, 1.0)
+    sf.render(buffer)
+    stream.write(buffer.astype(np.float32).tobytes())
+
+    # 1 second pan left with pitchbend down 2 semitones
+    sf.set_channel_pan(0, 0)
+    sf.set_channel_pitch_wheel(0, 0)
+    sf.render(buffer)
+    stream.write(buffer.astype(np.float32).tobytes())
+
+    # 1 second fade-out
+    sf.note_off()
+    sf.render(buffer)
+    stream.write(buffer.astype(np.float32).tobytes())
+
+    stream.close()
+    p.terminate()
+
 def test_all():
     test_help()
     test_load()
-    test_chord()
+    test_wav()
+    test_blocking()
 
 test_all()
